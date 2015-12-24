bc322be1fa34faa5969386fd7abf05c896c67e2c
Michael Hucka <mhucka@caltech.edu>
2015-12-23 19:20:20 -0800

    Improved robustness against bad content in GHTorrent CSV file.

--- misc-scripts/import-ghtorrent.py
+++ misc-scripts/import-ghtorrent.py
@@ -53,57 +53,68 @@ namestart = len('https://api.github.com/repos/')
 with open('projects.csv') as f:
     reader = csv.reader(f, escapechar='\\')
     count = 0
-    for row in reader:
+    failures = 0
+    while failures < 5:
         try:
-            key     = row[fields['url']][namestart:]
-            desc    = row[fields['description']]
-            lang    = row[fields['language']]
-            fork    = row[fields['forked_from']]
-            deleted = row[fields['deleted']]
+            for row in reader:
+                key     = row[fields['url']][namestart:]
+                desc    = row[fields['description']]
+                lang    = row[fields['language']]
+                fork    = row[fields['forked_from']]
+                deleted = row[fields['deleted']]
+
+                if not key in dbroot:
+                    msg('Unknown project {}'.format(key))
+                    continue
+                else:
+                    old = dbroot[key]
+                    already_has_language = False
+                    try:
+                        lang_id = Language.identifier(lang)
+                    except:
+                        msg('Unrecognized language {} -- skipping {}'.format(lang, key))
+                        continue
+                    if old.languages:
+                        if not lang_id in old.languages:
+                            old.languages.append(lang_id)
+                        else:
+                            msg('Already know {} has language {}'.format(key, lang))
+                            already_has_language = True
+                    else:
+                        old.languages = [lang_id]
+
+                    is_copy=True if fork != 'N' else False
+                    is_deleted=True if deleted != '0' else False
+
+                    if already_has_language and not is_copy:
+                        # Don't need to update the entry for either language or copy
+                        # info, so we can skip it altogether.
+                        continue
+
+                    msg('Updating {} for {}, is_copy {}'.format(key, lang, is_copy))
+                    n = RepoEntry(host=old.host,
+                                  id=old.id,
+                                  path=old.path,
+                                  description=old.description,
+                                  readme=old.readme,
+                                  copy_of=is_copy,
+                                  owner=old.owner,
+                                  owner_type=old.owner_type,
+                                  languages=old.languages,
+                                  deleted=is_deleted,
+                                  topics=old.topics,
+                                  categories=old.categories)
+                    dbroot[key] = n
+                    count += 1
+                    failures = 0
+                if count % 100 == 0:
+                    transaction.commit()
+                    msg('{} [{:2f}]'.format(count, time() - start))
+                    start = time()
         except Exception as ex:
             msg('Skipping {} because of parsing error: {}'.format(key, ex))
+            failures += 1
             continue
 
-        if not key in dbroot:
-            msg('Unknown project {}'.format(key))
-            continue
-        else:
-            old = dbroot[key]
-            try:
-                lang_id = Language.identifier(lang)
-            except:
-                msg('Unrecognized language {} -- skipping'.format(lang))
-                continue
-            if old.languages:
-                if not lang_id in old.languages:
-                    old.languages.append(lang_id)
-                else:
-                    msg('Already know {} has language {}'.format(key, lang))
-                    continue            # We already know about it.
-            else:
-                old.languages = [lang_id]
-
-            is_copy=True if fork != 'N' else False
-            is_deleted=True if deleted != '0' else False
-
-            msg('Updating {} to add {}'.format(key, lang))
-            n = RepoEntry(host=old.host,
-                          id=old.id,
-                          path=old.path,
-                          description=old.description,
-                          readme=old.readme,
-                          copy_of=is_copy,
-                          owner=old.owner,
-                          type=old.owner_type,
-                          languages=old.languages,
-                          deleted=is_deleted,
-                          topics=old.topics,
-                          categories=old.categories)
-            dbroot[key] = n
-            count += 1
-        if count % 10 == 0:
-            transaction.commit()
-            msg('{} [{:2f}]'.format(count, time() - start))
-            start = time()
     transaction.commit()
     # update_progress(1)

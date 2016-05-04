f8c90f9ac26ebbeb41a111293d4b62cfbe30ed5f
Michael Hucka <mhucka@caltech.edu>
2016-05-03 19:23:55 -0700

    Code to recreate indexes.

---
+++ reindex.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3.4
+
+import os
+import pprint
+import sys
+import time
+
+sys.path.append('../common')
+
+from casicsdb import *
+from pymongo import ASCENDING, DESCENDING, TEXT
+
+casicsdb = CasicsDB()
+github_db = casicsdb.open('github')
+repos = github_db.repos
+
+# start = time.time()
+# repos.create_index( [('owner', TEXT), ('name', TEXT)], background=True)
+# print(time.time() - start)
+
+# start = time.time()
+# repos.create_index( [('description', TEXT), ('readme', TEXT)], background=True)
+# print(time.time() - start)
+
+# start = time.time()
+# repos.create_index( [('languages.name', ASCENDING )], background=True)
+# print(time.time() - start)
+
+# start = time.time()
+# repos.create_index( [('created', ASCENDING )], background=True)
+# print(time.time() - start)
+
+# start = time.time()
+# repos.create_index( [('refreshed', ASCENDING )], background=True)
+# print(time.time() - start)
+
+# start = time.time()
+# repos.create_index( [('is_deleted', ASCENDING )], background=True)
+# print(time.time() - start)
+
+# start = time.time()
+# repos.create_index( [('is_fork', ASCENDING )], background=True)
+# print(time.time() - start)
+
+start = time.time()
+repos.create_index( [('is_visible', ASCENDING )], background=True)
+print(time.time() - start)

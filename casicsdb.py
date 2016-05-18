9fa0328407f9c0e3d102ecc6dcce2c6c2c296b95
Michael Hucka <mhucka@caltech.edu>
2016-05-18 11:59:49 -0700

    Overhauled the fields in the entries.
    Former-commit-id: 0af9d5a320ee403bf41955491cddf87b3e5bed6f

--- casicsdb.py
+++ casicsdb.py
@@ -30,9 +30,9 @@
 #
 #   repos = github_db.repos
 #
-# And that's it -- the variable `repos` is a MongoDB collection with normal
-# MongoDB operations available.  The first thing to know is that every GitHub
-# repository in this collection has a unique numeric identifier.  The
+# And that's it -- the variable `repos` will be a MongoDB collection with
+# normal MongoDB operations available.  The first thing to know is that every
+# GitHub repository in this collection has a unique numeric identifier.  The
 # database is set up to use that as the value of the MongoDB `_id` field.  So
 # to find a repo with the id 16335, you can do
 #
@@ -45,9 +45,8 @@
 #
 #   results = repos.find( {'languages.name': 'Python' } )
 #
-# This time, the value of `results` will be an iterator (a "Cursor" in
-# pymongo).  You can iterate over each result in a standard Python way such
-# as
+# This time, the value of `results` will be an iterator (known as a "Cursor" in
+# Mongo).  You can iterate over each result in a standard Python way such as
 #
 #   for entry in results:
 #      ... do something with entry ...
@@ -85,14 +84,21 @@
 # description      string      description field in GitHub entry
 # readme           string      README file in GitHub, as plain text
 # languages        array       has the form [{'name': 'C'}, {'name': 'R'}, ...]
-# created          date        time of creation in GitHub, according to them
-# refreshed        date        time of our last update for this entry
+# licenses         array       has the form [{'acronym': 'LGPL'}, ...]
+# topics           array       TBD
+# functions        array       TBD
+# time             dictionary  has the form
+#                                {'repo_created': timestamp,
+#                                 'repo_modified': timestamp,
+#                                 'data_refreshed': timestamp }
+# fork             dictionary  has the form
+#                                {'parent': 'someOwner/someName',
+#                                 'root': 'someOtherOwner/someOtherName' }
 # is_deleted       bool        whether it's now listed by GitHub as deleted
 # is_visible       bool        False if private or not visible for any reason
-# is_fork          bool        empty if unknown, true if known to be fork
-# fork_of          string      a repo_id if we know what this is a fork of
 # default_branch   string      default branch according to GitHub
-# archive_url      string      archive URL according to GitHub
+# homepage         string      not the github.com page, but a different one, if
+#                              a project's record in GitHub lists one
 #
 # In our database, the name of a user in a repo entry is also the key of that
 # user in collection 'users'.  So to find out more info about a user beyond
@@ -271,38 +277,119 @@ class CasicsDB():
 
 # This preallocates fields with data objects, for database efficiency.  It's
 # not required when using Mongodb, but doing this is said to improve efficiency
-# when Mongo updates a record, reducing the probability that it will have to
-# relocate the data object because it doesn't fit in its existing location.
+# when Mongo updates a record by reducing the probability it will have to
+# relocate the data object due to lack of space in its existing location.
 # The actual sizes of the fields can be anything once the real document is
 # created -- they are not at all constrained to the sizes allocated here.
 
-def repo_entry(id, name='-'*16, owner='-'*16, description='-'*512,
-               readme='-'*2048, languages=[],
+def repo_entry(id,
+               name='-'*16,
+               owner='-'*16,
+               description='-'*512,
+               readme='-'*2048,
+               languages=[],
+               licenses=[],
+               topics=[],
+               functions=[],
+               default_branch=None,
+               homepage=None,
+               is_deleted=None,
+               is_visible=None,
+               is_fork=None,
+               fork_of=None,
+               fork_root=None,
                created=canonicalize_timestamp(0.0),
-               refreshed=canonicalize_timestamp(0.0),
-               is_deleted=None, is_visible=None, is_fork=None, fork_of=None,
-               default_branch=None, archive_url=None):
-    '''Create a repo record, blank-padded if field values are not given.'''
+               last_modified=canonicalize_timestamp(0.0),
+               data_refreshed=canonicalize_timestamp(0.0)
+              ):
+    '''Create a repo record, blank-padded if field values are not given.
+    Some explanations about the fields:
 
+     'is_visible' is False for entries that we discovered somehow (perhaps
+      during a past indexing run, or perhaps from a GHTorrent dump) but we can
+      no longer access.  This may happen for any of several reasons:
+        - GitHub returns http code 451 (access blocked)
+        - GitHub returns code 403 (often "problem with this repository on disk")
+        - GitHub returns code 404 when you go to the repos' page on GitHub
+        - we find out it's a private repo
+
+      'is_deleted' is True if we know that a repo is reported to not exist in
+      GitHub.  This is different from 'is_visible' because we don't always
+      know whether something has been deleted -- sometimes all we know is
+      that we can't find it on github.com, yet another source (such as the
+      API) still reports it exists.  Another case where we mark something as
+      is_deleted == False but is_visible == True is when we get a code 451 or
+      403 during network accesses, because this means the repo entry does
+      still exist -- we just can't see it anymore.  Note that in all cases,
+      a value of is_deleted == True means is_visible == False.
+
+      'fork' has the value [] when we don't know whether a repo is a fork or
+      not; otherwise, the value is a dictionary with the following fields.
+        'parent': the parent repo from which this is a fork
+        'root': the original repo, if this is a fork of a fork
+      Note that the values of the fields in 'fork' can be None if we don't
+      know them, which can happen in cases when all we know is that a repo is
+      a fork but don't have more info than that.  Basically, what this means
+      is that callers should query against [] to find out if something is a
+      for or not, and when look for more details about forks, query fields
+      like 'fork.parent' or 'fork.root' to find out what we know.
+
+      'time' is a dictionary with the following fields; all values are in UTC
+      and are stored as floating point numbers:
+        'repo_created': time stamp for the creation of the repo on GitHub
+        'repo_modified': last modification on GitHub (if known)
+        'data_refreshed': when we last updated this record in our database
+
+      'languages' is a list of programming languages found in the source code
+      of the repository.  The field can have one of three possible values:
+        - an empty array: this means we have not tried to get language info
+        - the value -1: we tried to get language info but Github didn't give it
+        - a non-empty array: the languages used in the repo (see below)
+
+      The case of languages == -1 can happen for real repositories that have
+      files.  Sometimes GitHub just doesn't seem to record language info for
+      some repos, and other times, the repository is empty or has no
+      programming language files per se.  We mark these cases as -1 in our
+      database so that we don't keep trying fruitlessly to retrieve the info
+      when it's not there, but it does not mean that the repo does not have
+      files that could be analyzed -- it *might*.  We just know that we
+      couldn't get it the last time we tried.
+
+      When the value is an array of languages, it has this structure:
+        'languages': [{'name': 'Python'}, {'name': 'Java'}]
+      Currently, all we store with each language is the name, but we may
+      expand this in the future.  To use operators such as find() on the
+      language field, the query must use dotted notation.  Example:
+         results = repos.find( {'languages.name': 'Java' } )
+      To find all repositories that mention *either* of two or more languages,
+      a convenient approach is to use the "$in" operator:
+         results = repos.find( {'languages.name': { '$in': ['Matlab', 'C'] }} )
+
+      'homepage' is the URL of a home page (usually a GitHub Pages page) if
+      it is known.  This is NOT the path to the page on github.com -- it is a
+      different home page for the project, if it has one.  Most entries in
+      GitHub don't seem to have a value for this, and GitHub doesn't even
+      seem to provide a direct way to set this field from the user-level GUI.
+      It seems to get set only by creating a GitHub Pages site for a project,
+      or else maybe using the API directly.
+    '''
+    fork_field = [] if not is_fork else {'parent' : fork_of, 'root' : fork_root}
     entry = {'_id'             : id,
              'owner'           : owner,
              'name'            : name,
              'description'     : description,
              'readme'          : readme,
              'languages'       : languages,
-             'created'         : created,
-             'refreshed'       : refreshed,
+             'licenses'        : licenses,
+             'topics'          : topics,
+             'functions'       : functions,
              'is_visible'      : is_visible,
              'is_deleted'      : is_deleted,
-             'is_fork'         : is_fork,
-             'fork_of'         : fork_of,
+             'fork'            : fork_field,
+             'time'            : {'repo_created'   : created,
+                                  'repo_modified'  : last_modified,
+                                  'data_refreshed' : data_refreshed },
              'default_branch'  : default_branch,
-             'archive_url'     : archive_url }
+             'homepage'        : homepage
+            }
     return entry
-
-# 'is_visible' is False when we can't read it or reach it, which may be
-# for any of several reasons:
-# - github returns http code 451 (access blocked)
-# - github returns code 403 (usually "problem with this repository on disk")
-# - github returns code 404 when you try to go to the page on github
-# - we find out it's a private repo somehow

05ab6f0e58b1d2a8b6adf06e46aa243905120a17
Michael Hucka <mhucka@caltech.edu>
2016-08-02 12:13:02 -0700

    Template database connection code. Useful for interactive debugging.
    Former-commit-id: e293bc35383c27c4592a757245dda78965c4aa0b

---
+++ connect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.4
+
+import bson
+import json
+import os
+import pprint
+import sys
+import time
+
+sys.path.append('../common')
+from casicsdb import *
+from utils import *
+
+casicsdb = CasicsDB()
+github_db = casicsdb.open('github')
+repos = github_db.repos
